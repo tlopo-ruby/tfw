b4b58ec187d9a2b1151325f17708f9541c91f8e8
Tiago Lopo <tiago_lopo_da_silva@external.mckinsey.com>
2020-03-02 12:54:56 +0000

    Escope State properly

--- lib/tfw/state.rb
+++ lib/tfw/state.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-# Singleton class to keep the stack
-class State
-  include Singleton
+module TFW
+  # Singleton class to keep the stack
+  class State
+    include Singleton
 
-  def initialize
-    @stack = TFDSL::Stack.new
-  end
+    def initialize
+      @stack = TFDSL::Stack.new
+    end
 
-  def stack(&block)
-    @stack.instance_eval(&block) if block_given?
-    @stack
-  end
+    def stack(&block)
+      @stack.instance_eval(&block) if block_given?
+      @stack
+    end
 
-  def reset
-    @stack = TFDSL::Stack.new
+    def reset
+      @stack = TFDSL::Stack.new
+    end
   end
 end
